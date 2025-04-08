7a6e057a2895beaa450e2c66ea1eb64fd48be031
P. Pavithra Dharshini <dharshini172815@gmail.com>
2025-04-08 22:07:00 +0530

    UnionArray.java

--- mm_programs/UnionArray.java
+++ mm_programs/UnionArray.java
@@ -1,46 +1,65 @@
 package mm_programs;
-import java.util.Arrays;
+
 import java.util.Scanner;
-import java.util.HashSet;
+
 public class UnionArray {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter size of array 1 : ");
-		int s1 = sc.nextInt();
-		int[] arr1 = new int[s1];
-		System.out.println("Enter " +s1+ " elements : ");
-		for(int i=0; i<arr1.length; i++) {
-			arr1[i]=sc.nextInt();
-		}
-		
-		System.out.println("Enter size of array 2 : ");
-		int s2 = sc.nextInt();
-		int[] arr2 = new int[s2];
-		System.out.println("Enter " +s2+ " elements : ");
-		for(int i=0; i<arr2.length; i++) {
-			arr2[i]=sc.nextInt();
-		}
-		
-		System.out.println("Array 1 : " +Arrays.toString(arr1));
-		System.out.println("Array 2 : " +Arrays.toString(arr2));
-		
-		HashSet<Integer> set = new HashSet<>();
-		for(int i : arr1) {
-			set.add(i);
-		}
-		for(int j : arr2) {
-			set.add(j);
-		}
-		
-		int[] unionArr = new int[set.size()];
-		int index = 0;
-        for (int num : set) {
-            unionArr[index++] = num;
-        }
-        
-        Arrays.sort(unionArr);
-        System.out.println("Union Array is " +Arrays.toString(unionArr));
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        System.out.print("Enter size of array 1: ");
+        int s1 = sc.nextInt();
+        int[] arr1 = new int[s1];
+        System.out.println("Enter " + s1 + " elements:");
+        for (int i = 0; i < s1; i++) {
+            arr1[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter size of array 2: ");
+        int s2 = sc.nextInt();
+        int[] arr2 = new int[s2];
+        System.out.println("Enter " + s2 + " elements:");
+        for (int i = 0; i < s2; i++) {
+            arr2[i] = sc.nextInt();
+        }
+
+        int[] temp = new int[s1 + s2];
+        int k = 0;
+
+        for (int i = 0; i < s1; i++) {
+            if (!isPresent(temp, k, arr1[i])) {
+                temp[k++] = arr1[i];
+            }
+        }
+
+        for (int i = 0; i < s2; i++) {
+            if (!isPresent(temp, k, arr2[i])) {
+                temp[k++] = arr2[i];
+            }
+        }
+
+        for (int i = 0; i < k - 1; i++) {
+            for (int j = 0; j < k - i - 1; j++) {
+                if (temp[j] > temp[j + 1]) {
+                    int tempSwap = temp[j];
+                    temp[j] = temp[j + 1];
+                    temp[j + 1] = tempSwap;
+                }
+            }
+        }
+
+        System.out.print("Union Array is: ");
+        for (int i = 0; i < k; i++) {
+            System.out.print(temp[i] + " ");
+        }
+    }
+
+    public static boolean isPresent(int[] arr, int len, int num) {
+        for (int i = 0; i < len; i++) {
+            if (arr[i] == num) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
